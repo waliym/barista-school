3125637892b19793bb61181b22d28731e629147f
waliym <73928112+waliym@users.noreply.github.com>
2022-11-08 09:38:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+/* Когда пользователь нажимает на кнопку, переключаться раскрывает содержимое */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function myFunc() {
+  document.getElementById("myDropdown_2").classList.toggle("show");
+}
+
